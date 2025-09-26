1ffa51e1f5ce169d2708aa83115ce993c10c8b52
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2025-09-26 14:13:31 +0700

    Minor fix

--- app/user/home/components/recommendations.tsx
+++ app/user/home/components/recommendations.tsx
@@ -66,57 +66,55 @@ const Recommendations = () => {
     }, []);
 
     return (
-        <div>
-            <section className='flex flex-col w-full p-4 gap-2'>
-                <div className='flex flex-row justify-between'>
-                    <div className='flex flex-row'>
-                        <p className='text-2xl font-bold hover:underline hover:cursor-pointer'>Recommendation</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-7 mt-[1.5px]">
-                            <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-
-                    {/* Date Sort */}
-                    <div className='flex flex-row gap-1.5'>
-                        <Button variant="outline" size="icon" className="size-8 focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 hover:ring-1 hover:ring-blue-400 hover:cursor-pointer">
-                            <CalendarCog />
-                        </Button>
-                    </div>
+        <section className='flex flex-col w-full p-4 gap-2'>
+            <div className='flex flex-row justify-between'>
+                <div className='flex flex-row'>
+                    <p className='text-2xl font-bold hover:underline hover:cursor-pointer'>Recommendation</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-7 mt-[1.5px]">
+                        <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                    </svg>
                 </div>
-                {loading && <p>Loading your reads...</p>}
-                {error && <p className="text-red-500">{error}</p>}
 
-                {/* Card Sections */}
-                <div ref={recommendedContainerRef} className='flex flex-row gap-2 overflow-x-auto flex-nowrap pr-1 [&::-webkit-scrollbar]:hidden'>
-                    {!loading && !error && recommendedReads.map((read) => (
-                        // The key prop is essential for React lists!
-                        <div key={read.id} className="max-w-60">
-                            <a href="#" className="relative block h-75 overflow-hidden rounded-md">
-                                <img src={read.imageUrl} alt={`Cover for ${read.title}`} />
-                                <Button variant="outline" size="icon" className="absolute size-8 top-2 left-2 bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 hover:cursor-pointer">
-                                    <Settings2 />
-                                </Button>
+                {/* Date Sort */}
+                <div className='flex flex-row gap-1.5'>
+                    <Button variant="outline" size="icon" className="size-8 focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 hover:ring-1 hover:ring-blue-400 hover:cursor-pointer">
+                        <CalendarCog />
+                    </Button>
+                </div>
+            </div>
+            {loading && <p>Loading your reads...</p>}
+            {error && <p className="text-red-500">{error}</p>}
+
+            {/* Card Sections */}
+            <div ref={recommendedContainerRef} className='flex flex-row gap-2 overflow-x-auto flex-nowrap pr-1 [&::-webkit-scrollbar]:hidden'>
+                {!loading && !error && recommendedReads.map((read) => (
+                    // The key prop is essential for React lists!
+                    <div key={read.id} className="max-w-60">
+                        <a href="#" className="relative block h-75 overflow-hidden rounded-md">
+                            <img src={read.imageUrl} alt={`Cover for ${read.title}`} />
+                            <Button variant="outline" size="icon" className="absolute size-8 top-2 left-2 bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 hover:cursor-pointer">
+                                <Settings2 />
+                            </Button>
+                        </a>
+                        <div className='mt-2 p-2 rounded-md hover:bg-gray-800'>
+                            <a href="#">
+                                <h5 className="mb-1 text-md font-bold tracking-tight text-gray-900 dark:text-white max-w-180 truncate" title={read.title}>{read.title}</h5>
                             </a>
-                            <div className='mt-2 p-2 rounded-md hover:bg-gray-800'>
-                                <a href="#">
-                                    <h5 className="mb-1 text-md font-bold tracking-tight text-gray-900 dark:text-white max-w-180 truncate" title={read.title}>{read.title}</h5>
-                                </a>
-                                <p className="mb-3 text-sm font-normal text-gray-700 dark:text-gray-400">{read.lastRead}</p>
-                                <div className='flex flex-row justify-between text-sm font-normal text-gray-700 dark:text-gray-400'>
-                                    <div className='flex flex-row gap-1'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={statusColorMap[read.status] || 'text-gray-400 size-4 mt-0.5'}>
-                                        <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-                                        </svg>
-                                        <p>{read.status}</p>
-                                    </div>
-                                    <p>{read.chapter}</p>
+                            <p className="mb-3 text-sm font-normal text-gray-700 dark:text-gray-400">{read.lastRead}</p>
+                            <div className='flex flex-row justify-between text-sm font-normal text-gray-700 dark:text-gray-400'>
+                                <div className='flex flex-row gap-1'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={statusColorMap[read.status] || 'text-gray-400 size-4 mt-0.5'}>
+                                    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+                                    </svg>
+                                    <p>{read.status}</p>
                                 </div>
+                                <p>{read.chapter}</p>
                             </div>
                         </div>
-                    ))}
-                </div>
-            </section>
-        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
 
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -17,48 +17,46 @@ const NavBar = () => {
     const { setTheme } = useTheme();
     
     return (
-        <div>
-            <nav className="bg-white dark:bg-gray-900 border-b-2 border-gray-200 dark:border-gray-700">
-                <div className="flex flex-wrap items-center justify-between mx-auto p-3">
-                    <a href="/user/home" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                    </a>
-                    <div className="flex">
-                        <Input type="text" placeholder="Search" className='hidden md:block border-2 dark:focus:border-blue-500'/>
-                    </div>
-                    <div className="flex gap-2">
-                        <Button variant="outline" size="icon" className="md:hidden border-2 dark:focus:border-blue-500 hover:cursor-pointer">
-                            <Search />
-                        </Button>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <Button variant="outline" size="icon" className='focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 hover:ring-1 hover:ring-blue-400 hover:cursor-pointer'>
-                                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 hover:cursor-pointer" />
-                                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 hover:cursor-pointer" />
-                                    <span className="sr-only hover:cursor-pointer">Toggle theme</span>
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                                <DropdownMenuItem onClick={() => setTheme("light")}>
-                                Light
-                                </DropdownMenuItem>
-                                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                                Dark
-                                </DropdownMenuItem>
-                                <DropdownMenuItem onClick={() => setTheme("system")}>
-                                System
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                        <Avatar className='size-[34px] hover:cursor-pointer hover:ring-2 ring-blue-500 rounded-sm'>
-                            <AvatarImage src="/pictures/profile-pic.jpg" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                    </div>
+        <nav className="sticky top-0 z-50 bg-white dark:bg-gray-900 border-b-2 border-gray-200 dark:border-gray-700">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-3">
+                <a href="/user/home" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                </a>
+                <div className="flex">
+                    <Input type="text" placeholder="Search" className='hidden md:block border-2 dark:focus:border-blue-500'/>
                 </div>
-            </nav>
-        </div>
+                <div className="flex gap-2">
+                    <Button variant="outline" size="icon" className="md:hidden border-2 dark:focus:border-blue-500 hover:cursor-pointer">
+                        <Search />
+                    </Button>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon" className='focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 hover:ring-1 hover:ring-blue-400 hover:cursor-pointer'>
+                                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 hover:cursor-pointer" />
+                                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 hover:cursor-pointer" />
+                                <span className="sr-only hover:cursor-pointer">Toggle theme</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                            Light
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                            Dark
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("system")}>
+                            System
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <Avatar className='size-[34px] hover:cursor-pointer hover:ring-2 ring-blue-500 rounded-sm'>
+                        <AvatarImage src="/pictures/profile-pic.jpg" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </div>
+            </div>
+        </nav>
     )
 }
 
