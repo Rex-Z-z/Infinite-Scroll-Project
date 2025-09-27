b0918223fa24a38949bd649c1787df48064cfc44
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2025-09-27 20:02:00 +0700

    Update Components

--- app/user/home/components/recent-reads.tsx
+++ app/user/home/components/recent-reads.tsx
@@ -71,7 +71,9 @@ const RecentReads = () => {
         <section className='flex flex-col w-full p-4 gap-2'>
             <div className='flex flex-row justify-between'>
                 <div className='flex flex-row'>
-                    <p className='text-2xl font-bold hover:underline hover:cursor-pointer'>Recent Read</p>
+                    <p className='text-2xl font-bold hover:underline hover:cursor-pointer'>
+                        <a href="/user/library">Recent Read</a>
+                    </p>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-7 mt-[1.5px]">
                         <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                     </svg>
--- app/user/library/page.tsx
+++ app/user/library/page.tsx
@@ -1,9 +1,13 @@
+import NavBar from '@/components/ui/navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <h1 className='text-3xl font-bold'>Library Page</h1>
+    <div>
+      <NavBar />
+      <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-3xl font-bold'>Library Page</h1>
+      </div>
     </div>
   )
 }
