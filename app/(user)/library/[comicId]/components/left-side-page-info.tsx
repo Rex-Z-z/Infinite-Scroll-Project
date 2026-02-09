aaca9539dddb2b040655e2d109806ac0dae3a79d
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2026-02-09 19:39:00 +0700

    update(ui): split component

--- app/(user)/library/[comicId]/components/left-side-page-info.tsx
+++ app/(user)/library/[comicId]/components/left-side-page-info.tsx
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import LastReadDatePicker from './lastread-datepicker';
-import { Label } from '@/components/ui/label';
+import SwitchComicImage from './switch-comic-image';
 
 const ratingColorMap: { [key: string]: string } = {
     "Absolute Cinema": "bg-blue-400 hover:bg-blue-500",
@@ -80,27 +80,7 @@ const LeftSidePage = ({ comicId, data }: { comicId: string, data: ReadItem }) =>
     return (
         <div className="flex-none flex-col w-75 h-full">
             <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-                <DialogContent className="sm:max-w-xl">
-                    <DialogHeader>
-                        <DialogTitle>Comic Cover</DialogTitle>
-                        <DialogDescription>
-                            Select a cover image for your comic
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid grid-cols-3 gap-1">
-                        {[...Array(5)].map((_, index) => (
-                            <div key={index} className='flex h-60 items-center justify-center bg-gray-700 hover:bg-gray-800 rounded-md shadow-lg'>
-                                <Image size={25} className='text-gray-500' />
-                            </div>
-                        ))}
-                    </div>
-                    <DialogFooter className="sm:justify-start">
-                        <DialogClose asChild>
-                            <Button type="button">Close</Button>
-                        </DialogClose>
-                        <Button variant="default" className='text-black dark:text-white bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 hover:cursor-pointer'>Upload</Button>
-                    </DialogFooter>
-                </DialogContent>
+                <SwitchComicImage />
             </Dialog>
             
             {/* Cover */}
--- app/(user)/library/[comicId]/components/switch-comic-image.tsx
+++ app/(user)/library/[comicId]/components/switch-comic-image.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Image } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 
 const SwitchComicImage = () => {
     return (
-        <div>
-        
-        </div>
+        <DialogContent className="sm:max-w-xl">
+            <DialogHeader>
+                <DialogTitle>Comic Cover</DialogTitle>
+                <DialogDescription>
+                    Select a cover image for your comic
+                </DialogDescription>
+            </DialogHeader>
+            <div className="grid grid-cols-3 gap-1">
+                {[...Array(5)].map((_, index) => (
+                    <div key={index} className='flex h-60 items-center justify-center bg-gray-700 hover:bg-gray-800 rounded-md shadow-lg'>
+                        <Image size={25} className='text-gray-500' />
+                    </div>
+                ))}
+            </div>
+            <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                    <Button type="button">Close</Button>
+                </DialogClose>
+                <Button variant="default" className='text-black dark:text-white bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 hover:cursor-pointer'>Upload</Button>
+            </DialogFooter>
+        </DialogContent>
     )
 }
 
