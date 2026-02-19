cdf15b60eabc534b5d78f31fc5d4bad0d8f0934b
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2026-02-19 10:42:28 +0700

    update(ui): make login form responsive

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from '@/components/ui/navbar'
 import RecentReads from './components/recent-reads';
 import Recommendations from './components/recommendations';
 import LibraryRead from './components/library';
--- components/ui/login-form.tsx
+++ components/ui/login-form.tsx
@@ -8,37 +8,37 @@ import { Label } from '@/components/ui/label';
 const LoginForm = () => {
   return (
     <section>
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
             <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                 Flowbite
             </a>
-            <Card className="w-full sm:max-w-md">
+            <Card className="w-full sm:max-w-sm md:max-w-md">
                 <CardHeader>
-                    <CardTitle className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+                    <CardTitle className="text-md font-bold leading-tight tracking-tight md:text-2xl">
                         Sign in to your account
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
                     <div className="space-y-4 md:space-y-6">
                         <div className="grid w-full max-w-sm items-center gap-3">
-                            <Label htmlFor="email">Email</Label>
-                            <Input type="email" id="email" placeholder="Email" className="border-2 dark:focus:border-blue-500 hover:border-gray-400 hover:dark:border-gray-600" />
+                            <Label htmlFor="email" className='text-xs md:text-sm'>Email</Label>
+                            <Input type="email" id="email" placeholder="Email" className='text-xs md:text-sm'/>
                         </div>
                         <div className="grid w-full max-w-sm items-center gap-3">
-                            <Label htmlFor="email">Password</Label>
-                            <Input type="password" id="password" placeholder="Password" className="border-2 dark:focus:border-blue-500 hover:border-gray-400 hover:dark:border-gray-600" />
+                            <Label htmlFor="email" className='text-xs md:text-sm'>Password</Label>
+                            <Input type="password" id="password" placeholder="Password" className='text-xs md:text-sm'/>
                         </div>
                         <div className="flex items-center justify-between">
                             <div className="flex items-start gap-2">
-                                <Checkbox id="terms" className='border-2 data-[state=checked]:bg-blue-500 dark:data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700'/>
-                                <Label htmlFor="terms">Remember me</Label>
+                                <Checkbox id="terms"/>
+                                <Label htmlFor="terms" className='text-xs md:text-sm'>Remember me</Label>
                             </div>
-                            <Button variant="link">
+                            <Button variant="link" className='text-xs md:text-sm'>
                                 <a href="#">Forgot password?</a>
                             </Button>
                         </div>
-                        <Button variant="default" className="w-full text-black dark:text-white bg-blue-500 dark:bg-blue-600 hover:bg-blue-800 dark:hover:bg-blue-500 hover:ring-2 hover:ring-blue-500 focus:ring-2 focus:ring-blue-300 hover:cursor-pointer" asChild>
+                        <Button className="w-full hover:cursor-pointer" asChild>
                             <a href="/home">Sign in</a>
                         </Button>
                     </div>
